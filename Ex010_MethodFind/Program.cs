dd8f6a6772e173cbc5d97030b30b46393c398411
c8ler <kotovsergey@yandex.ru>
2022-05-14 13:07:23 +0300

    all renamed and new ex 008 - 012

---
+++ Ex010_MethodFind/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 21, 20, 3, 4, 95, 96, 72, 82, 29 };
+
+int n = array.Length;
+int find = 82;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ Ex011_ArrayRandom/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int len = collection.Length;
+    int ind = 0;
+    while (ind < len)
+    {
+        collection[ind] = new Random().Next(1, 10);
+        ind++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 3);
+Console.WriteLine(pos);
